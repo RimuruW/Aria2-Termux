39662f25d89d21c95472708dbc83e10fe2980dc1
RimuruW <59136309+RimuruW@users.noreply.github.com>
2020-10-13 23:31:51 +0800

    细节优化

--- aria2.sh
+++ aria2.sh
@@ -28,7 +28,7 @@ Error="[${Red_font_prefix}错误${Font_color_suffix}]"
 Tip="[${Green_font_prefix}注意${Font_color_suffix}]"
 
 check_root() {
-    [[ $EUID = 0 ]] && echo -e "${Error} 检测到您正在尝试用 ROOT 权限运行脚本\n这是不建议也不被允许的\n请勿在任何情况下以 ROOT 权限运行该脚本,以避免造成无法预料的损失" && return 0
+    [[ $EUID = 0 ]] && echo -e "${Error} 检测到您正在尝试用 ROOT 权限运行脚本\n这是不建议也不被允许的\n请勿在任何情况下以 ROOT 权限运行该脚本,以避免造成无法预料的损失" && exit 1
 }
 
 #检查系统
@@ -55,6 +55,30 @@ check_storage() {
     [[ ! -d "$HOME/storage/shared/Android/" ]] && echo -e "${Error} Termux 存权限获取失败，请检查！" && exit 1
 }
     
+check_mirrors() {
+	mirrors_status=$(cat $PREFIX/etc/apt/sources.list | grep -v '#' | grep "mirror")
+	if [ -z "$mirrors_status" ]; then
+		echo -e "${Info}  Termux 镜像源未配置！"
+		echo -e "对于国内用户，添加清华源作为镜像源可以有效增强 Termux 软件包下载速度"
+		echo -en "是否添加清华源？[y/n]"
+		read mirror_choose
+		case $mirror_choose in
+			y)
+				sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+				sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
+				sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
+				apt update && apt upgrade -y
+				;;
+			n)
+				echo -e "${Info} 使用默认源进行安装"
+				;;
+			*)
+				echo -e "${Info} 跳过，使用默认源进行安装"
+				;;
+		esac
+	fi
+
+}
 
 Download_aria2_conf() {
     PROFILE_URL1="https://one.qingxu.ga/onedrive/aira2"
@@ -88,7 +112,7 @@ move.sh
     sed -i "s@^\(dir=\).*@\1${download_path}@" ${aria2_conf}
     sed -i "s@/root/.aria2/@${aria2_conf_dir}/@" ${aria2_conf_dir}/{*.sh,aria2.conf}
     sed -i "s@^\(rpc-secret=\).*@\1$(date +%s%N | md5sum | head -c 20)@" ${aria2_conf}
-    echo "log=$aria2_log" >> ${aria2_conf}
+    sed -i "s/^#log=*/log=$aria2_log/" ${aria2_conf}
     touch aria2.session
     chmod +x *.sh
     echo -e "${Info} Aria2 配置文件下载完成！"
@@ -106,6 +130,7 @@ Install_aria2() {
 	check_root
 	[[ -e ${aria2c} ]] && echo -e "${Error} Aria2 已安装，请检查 !" && return 0
 	check_sys
+	check_mirrors
 	echo -e "${Info} 开始安装并配置依赖..."
 	Installation_dependency
 	echo -e "${Info} 开始下载并安装主程序..."
@@ -123,8 +148,10 @@ Start_aria2() {
 	check_installed_status
 	check_pid
 	[[ ! -z ${PID} ]] && echo -e "${Error} Aria2 正在运行，请检查 !" && return 0
-	check_storage
 	aria2c --conf-path=${aria2_conf} -D
+	check_storage
+	echo -e "${Info} 尝试开启唤醒锁…"
+	termux-wake-lock
 }
 Stop_aria2() {
 	check_installed_status
@@ -136,7 +163,10 @@ Restart_aria2() {
 	check_installed_status
 	check_pid
 	[[ ! -z ${PID} ]] && kill -9 ${PID}
+	check_storage
 	aria2c --conf-path=${aria2_conf} -D
+	echo -e "${Info} 尝试开启唤醒锁…"
+	termux-wake-lock
 }
 Set_aria2() {
 	check_installed_status
@@ -329,7 +359,6 @@ Set_aria2_vim_conf() {
  ${Green_font_prefix}2.${Font_color_suffix} 退出并保存文件：按 ${Green_font_prefix}Ctrl+X${Font_color_suffix} 组合键，输入 ${Green_font_prefix}y${Font_color_suffix} ，按 ${Green_font_prefix}Enter${Font_color_suffix} 键
  ${Green_font_prefix}3.${Font_color_suffix} 退出不保存文件：按 ${Green_font_prefix}Ctrl+X${Font_color_suffix} 组合键，输入 ${Green_font_prefix}n${Font_color_suffix}
  ${Green_font_prefix}4.${Font_color_suffix} nano 详细使用教程：${Green_font_prefix}https://p3terx.com/archives/linux-nano-tutorial.html${Font_color_suffix}
- ${Green_font_prefix}5.${Font_color_suffix} 配置文件有中文注释，若语言设置有问题会导致中文乱码
  "
     echo "按任意键继续，按 Ctrl+C 组合键取消"
     read var
@@ -395,10 +424,9 @@ View_Aria2() {
     LocalIP=$(
     for LOCALIP in $(ip a | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | cut -d "/" -f1)
     do
-	    echo $LOCALIP > localip.txt
+	    echo $LOCALIP > $TMPLOCALIP
     done
-    tail -n 1 localip.txt
-    rm -f localip.txt
+    echo $TMPLOCALIP
 )
     [[ -z "${IPV4}" ]] && IPV4="IPv4 地址检测失败"
     [[ -z "${IPV6}" ]] && IPV6="IPv6 地址检测失败"
