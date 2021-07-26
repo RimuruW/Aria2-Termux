01fa6526b97bde24e7a5ff7f39553f322870a003
RimuruW <ruomo85@gmail.com>
2021-07-26 20:23:13 +0800

    Add basic install.sh and atm

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!env bash
+#!/data/data/com.termux/files/usr/bin/bash
 
 RED=$(printf '\033[31m')
 GREEN=$(printf '\033[32m')
@@ -80,57 +80,66 @@ else
 	exit 1
 fi
 
-if [ -d "$PREFIX/etc/aria2" ]; then
+if [ -d "$PREFIX/etc/atm" ]; then
 	yellow "
 [!] 您已安装 Aria2-Termux ，无需重复安装
-如果您需要移除 Aria2-Termux，请输入 rm -rf $PREFIX/etc/aria2
+如果您需要移除 Aria2-Termux，请输入 rm -rf $PREFIX/etc/atm
 	"
 	exit 1
 fi
 
-	if [ -n "$(find /data/data/com.termux/cache/apt/pkgcache.bin -mmin -360 2>/dev/null)" ]; then
-		if [ -n "${current_mirror}" ]; then
-			echo -n "Checking availability of current mirror: "
-			if timeout 6 curl \
-				--head \
-				--fail \
-				--connect-timeout 5 \
-				--location \
-				--user-agent 'Termux-PKG/1.0 mirror-checker (termux-tools 0.112) Termux (com.termux; install-prefix:/data/data/com.termux/files/usr)' \
-				"${current_mirror%/}/dists/stable/Release" >/dev/null 2>&1; then
-					echo "ok"
-					return
-			else
-				echo "bad"
-			fi
-		fi
+hostname() {
+	echo "$1" | awk -F'[/:]' '{print $4}'
+}
+
+timeout_test() {
+	local URL="${1%/}"
+	local timeout="${2-5}"
+
+	timeout "$((timeout + 1))" curl \
+		--head \
+		--fail \
+		--connect-timeout "$timeout" \
+		--location \
+		--user-agent "Termux-PKG/1.0 mirror-checker (termux-tools 0.112) Termux (com.termux; install-prefix:/data/data/com.termux/files/usr)'" \
+		"$URL" >/dev/null 2>&1
+}
+
+replace_mirrors() {
+	red "[!] Termux 镜像源不可用!"
+	blue "对于国内用户，临时添加清华源作为镜像源可以有效增强 Termux 软件包下载速度"
+	if ask "是否临时添加清华源用以下载脚本依赖?" "Y"; then
+		cp "${PREFIX}"/etc/apt/sources.list "${PREFIX}"/etc/apt/sources.list.bak
+		sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' "$PREFIX/etc/apt/sources.list"
+		sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' "$PREFIX/etc/apt/sources.list.d/game.list"
+		sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' "$PREFIX/etc/apt/sources.list.d/science.list"
+		apt update && apt upgrade -y
+		USE_MIRROR=1
+	else
+		blue "使用默认源进行安装"
+	fi
+}
 
 check_mirrors() {
+	blue "[*] 检查网络环境及镜像源..."
 	local current_mirror
 	current_mirror=$(grep -P "^\s*deb\s+" /data/data/com.termux/files/usr/etc/apt/sources.list | grep -oP 'https?://[a-z0-9/._-]+')
 
-	if timeout 6 curl \
-		--head \
-		--fail \
-		--connect-timeout 5 \
-		--location \
-		--user-agent 'Termux-PKG/1.0 mirror-checker (termux-tools 0.112) Termux (com.termux; install-prefix:/data/data/com.termux/files/usr)' \
-		"${current_mirror%/}/dists/stable/Release" >/dev/null 2>&1; then
-		green "[√] 当前镜像源可用"
-		return 0
-	else
-		red "[!] Termux 镜像源不可用!"
-		blue "对于国内用户，添加清华源作为镜像源可以有效增强 Termux 软件包下载速度"
-		if ask "是否添加清华源?" "Y"; then
-			cp ${PREFIX}/etc/apt/sources.list ${PREFIX}/etc/apt/sources.list.bak
-			sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' "$PREFIX/etc/apt/sources.list"
-			sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' "$PREFIX/etc/apt/sources.list.d/game.list"
-			sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' "$PREFIX/etc/apt/sources.list.d/science.list"
-			apt update && apt upgrade -y
-			USE_MIRROR=1
+	if timeout_test "google.com"; then
+		if timeout_test "${current_mirror%/}/dists/stable/Release"; then
+			green "[√] 当前镜像源可用"
 		else
-			blue "使用默认源进行安装"
+			replace_mirrors
 		fi
+	elif [[ "$(hostname "$current_mirror")" == *".cn" ]]; then
+		if timeout_test "${current_mirror%/}/dists/stable/Release"; then
+			green "[√] 当前镜像源可用"
+		else
+			replace_mirrors
+
+		fi
+	else
+		replace_mirrors
 	fi
 }
 
@@ -156,8 +165,9 @@ apt-get upgrade -y
 
 # Clone files
 blue "\n[*] 正在拉取远程仓库..."
-git clone https://github.com/RimuruW/Aria2-Termux "$PREFIX/etc/aria2"
-cd "$PREFIX/etc/aria2" || {
+mkdir -p "$PREFIX/etc/atm"
+git clone https://github.com/RimuruW/Aria2-Termux "$PREFIX/etc/atm/main"
+cd "$PREFIX/etc/atm/main" || {
 	red "目录跳转失败！" >&2
 	exit 1
 }
@@ -169,16 +179,17 @@ cd "$HOME" || {
 }
 
 blue "\n[*] 正在创建启动器..."
-mv -f "$PREFIX/etc/aria2/bin/atm" "$PREFIX/bin/atm"
+mv -f "$PREFIX/etc/atm/main/bin/atm" "$PREFIX/bin/atm"
 chmod +x "$PREFIX/bin/atm"
 
 blue "\n[*] 正在处理配置文件..."
 mkdir -p "${HOME}/.config/local/atm"
-ln -s "${PREFIX}/etc/aria2" "${HOME}/.config/local/atm"
-[ -n ${USE_MIRROR} ] && mv -f ${PREFIX}/etc/apt/sources.list ${PREFIX}/etc/apt/sources.list
+ln -sf "${PREFIX}/etc/atm/main/conf" "${HOME}/.config/atm/conf"
+ln -sf "${PREFIX}/etc/atm/main/script" "${HOME}/.config/atm/script"
+[[ ${USE_MIRROR} ]] && mv -f "${PREFIX}"/etc/apt/sources.list.bak "${PREFIX}"/etc/apt/sources.list
 
 if [ -f "$PREFIX/bin/atm" ]; then
-	green "\n[√]  安装成功！请输入 aria2 启动脚本！"
+	green "\n[√]  安装成功！请输入 atm 启动脚本！"
 	exit 0
 else
 	red "
