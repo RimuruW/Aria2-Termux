29458fa3e8701e8f9f2ef188a6235f33f1469a60
RimuruW <59136309+RimuruW@users.noreply.github.com>
2021-08-16 17:36:18 +0800

    fix a bug

--- core/restart-aria2.sh
+++ core/restart-aria2.sh
@@ -1,4 +1,4 @@
-        #!/data/data/com.termux/files/usr/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
 check_installed_status
 check_pid
@@ -8,4 +8,5 @@ blue "[*] 尝试开启唤醒锁……"
 termux-wake-lock
 green "[√] 所有步骤执行完毕，开始启动..."
 "$PREFIX"/bin/aria2c "$(grep -v '#' "$HOME/.aria2/aria2.conf" | sed '/^$/d' | sed "s/^/--&/g" | sed ':label;N;s/\n/ /;b label')" -D
+check_pid
 [[ -z ${PID} ]] && red "[!] Aria2 启动失败，请检查日志！" && return 1
